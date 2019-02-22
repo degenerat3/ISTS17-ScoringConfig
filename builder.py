5203e99780f169393eab04fd236ad47076ab41f1
Micah J. Martin <mjm5097@rit.edu>
2019-02-22 04:12:16 -0500

    Getting this config to work

--- builder.py
+++ builder.py
@@ -1,23 +1,25 @@
 # Turn all those pesky configs into a massive giant config
 import yaml
 
-checks = ["Alexander", "Julius", "Churchhill", "Leonidas", "Attila", "Xerxes", "Ashoka",
-          "Gandhi", "Bonaparte", "Ataturk"]
+#checks = ["Alexander", "Julius", "Churchhill", "Leonidas", "Attila", "Xerxes", "Ashoka",
+          #"Gandhi", "Bonaparte", "Ataturk"]
+
+checks = ["Alexander", "Ashoka", "ataturk"]
 
 def build_team(team):
     """Build a team for the given team number
     """
     output = ""
     with open("base_team.yml") as inf:
-        data = inf.read()
+        data = inf.read().rstrip()
         output = data.replace("$TEAM", str(team)) # Change the team number
         output += "\n"
     for check in checks:
         check = check.lower()+".yml"
         with open("check_configs/"+check) as inf:
-            data = inf.read()
+            data = inf.read().rstrip()
             data = data.replace("$TEAM", str(team)) # Change the team number
-            output += data
+            output += data + "\n"
     return output
             
 
@@ -26,12 +28,17 @@ def build_all():
     with open("competition.yml", "w") as ofil:
         # Load the base config and write it to the outfile
         with open("base.yml") as inf:
-            ofil.write(inf.read())
+            ofil.write(inf.read().rstrip())
+            ofil.write("\n")
         # Iterate all the teams and add them to the outfile
-        for team in range(0, 14):
+        for team in range(TEAM_LOW, TEAM_HIGH+1):
             ofil.write(build_team(team))
         
     print("Config written to competition.yml")
 
 if __name__ == "__main__":
+    #TEAM_LOW = 0
+#    TEAM_HIGH = 13
+    TEAM_LOW = 1
+    TEAM_HIGH = 2
     build_all()
